86ad38d0f98c01328e257107807166684f4f8be5
Elena <freshjs.ftm@gmail.com>
2022-01-25 08:53:13 +0200

    Add Task

--- models/user.js
+++ models/user.js
@@ -3,13 +3,8 @@ const {Model} = require('sequelize');
 const {isBefore} = require('date-fns');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model { ///User  Users => users
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      User.hasMany(models.Task);
     }
   }
   User.init({
--- router.js
+++ router.js
@@ -1,11 +1,13 @@
 const {Router} = require('express');
 const UserController = require('./controllers/user.controller');
-
+const TaskController = require('./controllers/task.controller');
 const router = Router();
 router.post('/user', UserController.createUser);
 router.get('/users', UserController.getAllUsers);
 
 router.patch('/user/:id', UserController.updateUser);
 router.patch('/user-v2/:id', UserController.updateUserInstance);
+
+router.post('/user/:id/task', TaskController.createTask)
 
 module.exports = router;
