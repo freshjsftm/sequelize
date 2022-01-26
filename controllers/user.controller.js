870a12c2577490ed37fddedbb345fd95f87b6350
Elena <freshjs.ftm@gmail.com>
2022-01-26 10:10:46 +0200

    Add paginate

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -12,13 +12,15 @@ module.exports.createUser =  async (req, res, next) =>{
 
 module.exports.getAllUsers = async (req, res, next) =>{
   try {
+    const {pagination={}} = req;
     const users = await User.findAll({ 
       where:{
         //firstName:'Elon'
       },
       attributes: { 
         exclude: ['password'] 
-      }
+      },
+      ...pagination
     });
     res.status(200).send({data:users});
   } catch (error) {
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 const {Router} = require('express');
 const UserController = require('../controllers/user.controller');
 const {checkUser} = require('../middlewares/user.mw');
+const paginate = require('../middlewares/paginate.mw');
 
 const userRouter = Router();
 userRouter.post('/', UserController.createUser);
-userRouter.get('/', UserController.getAllUsers);
+userRouter.get('/',paginate, UserController.getAllUsers);
 //userRouter.get('/:userId', UserController.getUser);
 userRouter.patch('/:userId', UserController.updateUser);
 userRouter.patch('/v2/:userId', checkUser, UserController.updateUserInstance);
